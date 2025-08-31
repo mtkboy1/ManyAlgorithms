f0bf17ce38cf3a5ba915ca4cef74ab9e54d1609e
mtkboy1 <mrbrawler42@gmail.com>
2025-08-31 20:14:21 +0300

    modified:   src/Graph/search/Width.c 	modified:   src/structs/queue.c

--- src/structs/queue.c
+++ src/structs/queue.c
@@ -6,24 +6,17 @@ void init(struct queue* q){
 }
 void add(int i, struct queue* q){
     struct queue new_q;
-    //new_q.st = calloc(q->size,sizeof(int));
-    //printf("\n%i",q->st[0]);
     new_q.st = (int*) realloc(q->st,sizeof(int)*(q->size+1));
     new_q.size = q->size;
     new_q.size++;
     new_q.st[q->size] = i;
     *q = new_q;
-    //printf("%i",q->size);
-    //q->st[q->size-1] = i;
-    
 }
 void removeFrist(struct queue* q){
     struct queue new_q;
     new_q.st = (int*) calloc(q->size,sizeof(int));
-    //printf("\n%i",q->st[0]);
     new_q.st = (int*) realloc(q->st,sizeof(int)*(q->size-1));
     new_q.size = q->size;
-    //new_q.st[q->size] = i;
     *q = new_q;
     for(int i = 1; i<q->size; i++){
         new_q.st[i-1] = q->st[i];
